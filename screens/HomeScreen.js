39d5c8bcc36cc62b0873f296d24890a907c8fe9b
Arash Hosseini <arashhosseini733@gmail.com>
2023-04-22 03:33:58 +0330

    Setting Up React Native Navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View className="flex-1 bg-white">
+            <Text className="text-red-500">Open up App.js to start working on your app!</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
